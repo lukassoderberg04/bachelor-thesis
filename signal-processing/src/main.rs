bfe48bfb1937809bad436cd41c34b7e199ef6d28
Ludwig Granstedt <71264282+ludegra@users.noreply.github.com>
2026-02-25 00:23:59 +0100

    Documented functions and started creating helper functions for a coming refactor in preparation of real time streaming

--- signal-processing/src/main.rs
+++ signal-processing/src/main.rs
@@ -1,7 +1,7 @@
 mod udp_sender;
 use udp_sender::AudioUdpSender;
 
-use std::error::Error;
+use std::{collections::VecDeque, error::Error, sync::{atomic::{AtomicBool, Ordering}, mpsc::{Receiver, Sender}}};
 
 use ndarray::prelude::*;
 use num_complex::{ComplexFloat};
@@ -102,8 +102,6 @@ fn main() {
     fig.save_with_size("./junk/fft.png", 1280, 960)
         .unwrap();
 
-    
-
     // Convert the extracted amplitudes (f64) to f32 audio samples.
     let audio_samples: Vec<f32> = hp_amplitudes.iter().map(|&x| x as f32).collect();
 
@@ -129,14 +127,124 @@ fn static_pca(s: &ArrayView2<f64>) -> Result<Array1<f64>, Box<dyn Error>> {
     Ok(amplitudes)
 }
 
+/// Uses Oja's rule to perform PCA of data where the future of the dataset is unknown.
+/// 
+/// ## Parameters
+/// 
+/// - `weights` An `&mut Array1` with weights for each input component. 
+/// The array can have any (normalized) weights when the function is initially called, 
+/// but should be reused and untouched during the run. 
+/// 
+/// - `x` An `&ArrayView1` with the input data.
+/// 
+/// - `learning_rate` The rate of how fast the weights should be adjusted.
+/// 
+/// ## Rerurns
+/// 
+/// The magnitude along the principal component axis.
 fn ojas_rule(
     weights: &mut Array1<f64>,
-    normalised_stokes_vector: &ArrayView1<f64>,
+    x: &ArrayView1<f64>,
     learning_rate: f64,
 ) -> f64 {
-    let y = weights.dot(normalised_stokes_vector);
+    let y = weights.dot(x);
 
-    *weights = &*weights + learning_rate * y * (normalised_stokes_vector - &*weights * y);
+    *weights = &*weights + learning_rate * y * (x - &*weights * y);
+    *weights /= weights.norm(); // Normalize 
 
     y
+}
+
+/// Reads values from a channel, does a STFT on them and sends the resulting vectos through an output channel.
+/// 
+/// ## Parameters
+/// 
+/// - `rx` A reciever for the input channel.
+/// - `ty` A sender for the output channel.
+/// - `should_stop` An `AtomicBool` signifying weather the function should cease operations.
+/// - `window_size` The number of elements on which to perform each iteration of the STFT.
+/// - `hop_size` The number of elements between each iteration of the STFT.
+/// 
+/// ## Returns
+/// 
+/// The function never returns unless an error occurs or `should_stop` is set to `true`. 
+/// The values are instead returned through the output channel in the form of `Vec<f64>` 
+/// corresponding to the intensity at each frequency bin for one iteration.
+fn stft(rx: &Receiver<f64>, ty: Sender<Vec<f64>>, should_stop: &AtomicBool, window_size: usize, hop_size: usize) -> Result<(), Box<dyn Error>> {
+    let mut x = VecDeque::new();
+    let mut iter = 0usize..;
+
+    // Window size being 0 will cause logical errors
+    if window_size == 0 {
+        return Err("Window size can not be 0.".into());
+    }
+    
+    while should_stop.load(Ordering::Relaxed) {
+        let i = iter.next().unwrap(); // Will not return None
+
+        // Recieve a value from the channel and insert it into the deque.
+        x.push_back(rx.recv()?);
+
+        // Make sure enough samples have arrived and that the window is big enough.
+        if x.len() < window_size + 1 {
+            continue;
+        }
+
+        // Remove now irrelevant values. The previous guard clause asserts that the deque is not empty.
+        x.pop_front().unwrap();
+
+        // Only do the FFT when the hop size has been reached.
+        if i % hop_size != 0 {
+            continue;
+        }
+
+        // Rearrange the contents of the deque into a single slice and pass it to the fourier iteration.
+        let intensity_spectrum = stft_iteration(x.make_contiguous())?;
+
+        ty.send(intensity_spectrum)?;
+    }
+
+    Ok(())
+}
+
+/// Performs an iteration of the STFT with a few adjustments.
+/// 
+/// ## Parameters
+/// 
+/// - `x` A slice containing the values on which to perform the operation.
+/// 
+/// ## Returns
+/// 
+/// A vector of intensities represented as the magnitudes squared of the frequency spectrum.
+fn stft_iteration(x: &[f64]) -> Result<Vec<f64>, Box<dyn Error>> {
+    let n = x.len();
+
+    // Convert to Array1 to help with calculations
+    let x = Array1::from_iter(x.into_iter());
+
+    // Generate window coefficients
+    let window = hann(n);
+
+    let x_weighted = window * x;
+
+    // Perform the FFT on the weighted function
+    let spectrum = rfft(&x_weighted.as_slice().ok_or("Could not create a slice from `x_weigthed`.")?, Some(n))?;
+
+    // Return intensity as magnitude squared
+    Ok(spectrum.iter().map(|a| a.norm().powi(2)).collect::<Vec<f64>>())
+}
+
+/// Creates a hann window of a given length.
+/// 
+/// ## Parameters
+/// 
+/// - `n` Number of samples.
+/// 
+/// ## Returns
+/// 
+/// An `Array1<f64>` of samples of the hann window.
+fn hann(n: usize) -> Array1<f64> {
+    Array1::from_shape_fn(n, |i| {
+        0.5 * (1.0 - (2.0 * std::f64::consts::PI * i as f64 / (n as f64 - 1.0)).cos())
+    })
 }
