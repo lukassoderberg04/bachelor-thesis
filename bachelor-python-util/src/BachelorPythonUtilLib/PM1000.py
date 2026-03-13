b951e7b22dbcc7bc997d7a08413be779cbd61297
gottegubben <lukas.soderberg0404@gmail.com>
2026-03-13 14:28:34 +0100

    Implemented the PM1000 file result reader so that we now can read the file data. Has to create tests first!

---
+++ bachelor-python-util/src/BachelorPythonUtilLib/PM1000.py
@@ -0,0 +1,102 @@
+from pathlib import Path
+from io import TextIOWrapper
+from typing import Optional
+import re as regex
+
+"""
+    PM1000 module contains classes and functions that are important
+    for retrieving and communicating with the PM1000 polarimeter.
+"""
+
+"""
+    A class that can open a result file and retrieve the stokes vectors
+    and also the attributes of the recording (from a PM1000 recording).
+"""
+class PM1000ResultFileReader:
+    def __init__(self, filePath: Path, chunkSize: int) -> "PM1000ResultFileReader":
+        self._filePath                            = filePath
+        self._chunkSize                           = chunkSize
+        self._fileHandle: Optional[TextIOWrapper] = None
+        self._attributes: dict[str, str]          = {}
+
+        if not self._filePath.exists():
+            raise FileNotFoundError(f"The file in question wasn't found: {self._filePath}")
+        
+    """
+        Parses the headers and fills in the attribute list.
+    """
+    def _parseHeader(self):
+        """
+            ^     - Start of line.
+            \s*   - Match whitespaces (\s = whitespace, * = 0 or more).
+            (\w+) - Create a group, which is defined by parenthesis, which contains words (\w = letter).
+            =     - Just matches the equals token.
+            ['"]? - The square bracket means match one of these tokens in it. The question marks just states optional.
+            (.*?) - Match ANY token, like a joker. The question marks just states that it should stop when seeing the next pattern in the list.
+            ;     - Match this token.
+            $     - Marks end of line.
+        """
+        headerPattern: regex.Pattern = regex.compile(r"^ \s* (\w+) \s* = \s* ['\"]? (.*?) ['\"]? \s* ; \s* $")
+        
+        while True:
+            lastPos = self._fileHandle.tell()
+            line    = self._fileHandle.readline()
+
+            match   = headerPattern.match(line)
+
+            if match: # If a match was found:
+                key   = match.group(1)
+                value = match.group(2)
+
+                self._attributes[key] = value
+
+            else: # If no match was found... data begins:
+                self._fileHandle.seek(lastPos)
+                break
+    
+    """
+        Opens the designated file and parses the headers.
+        Now ready to read stokes parameter chunk by chunk.
+    """
+    def Open(self):
+        # Open a read handle to the file.
+        self._fileHandle = self._filePath.open("r", encoding="utf-8")
+
+        self._parseHeader()
+
+    """
+        Gets the next list of samples from the file.
+        Returns None if there's no more samples to read.
+    """
+    def GetNextSample(self) -> Optional[list[float]]:
+        if not self._fileHandle:
+            raise RuntimeError("File must be open before reading!")
+        
+        line = self._fileHandle.readline()
+        if not line or not line.strip():
+            self.Close()
+            return None
+        
+        return [float(x.strip()) for x in line.split(',')]
+    
+    """
+        Closes the handle to the file.
+    """
+    def Close(self):
+        if self._fileHandle:
+            self._fileHandle.close()
+            self._fileHandle = None
+    
+    """
+        If using 'with' on the class for reading,
+        makes sure that on enter it opens the file handle.
+    """
+    def __enter__(self): 
+        self.Open()
+    
+    """
+        If using 'with' on the class for reading,
+        makes sure that on leave it closes the file handle.
+    """
+    def __exit__(self, *args): 
+        self.Close()
